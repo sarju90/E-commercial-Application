271810fbdf8a3c8aa57dc3dc9cca2d687f1bbb03
sarju90 <78295505+sarju90@users.noreply.github.com>
2022-01-01 15:03:30 +0530

    Day1

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:ecommercial_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
