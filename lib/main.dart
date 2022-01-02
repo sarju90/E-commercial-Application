d24f727726a3761c9de9119a9b33153511a69b90
sarju90 <78295505+sarju90@users.noreply.github.com>
2022-01-02 12:11:36 +0530

    day 3 complated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommercial_app/pages/home_page.dart';
 import 'package:ecommercial_app/pages/login_Page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // themeMode: ThemeMode.dark,
+      // darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       routes: {
-        "/": (context) => Homepage(),
+        "/": (context) => Loginpage(),
         "/login": (context) => Loginpage(),
       },
     );
